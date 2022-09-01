957857634735182e3d62a6b2027ee78402f30b01
edupinhata <edupinhata@gmail.com>
2022-09-01 11:05:26 -0300

    Adding rotes and configuration in Sales Example application.

---
+++ vendas/src/main/java/io/github/edupinhata/VendasApplication.java
@@ -0,0 +1,32 @@
+package io.github.edupinhata;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @Value("${application.name}")
+    private String applicationNameProperties;
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationNameConfiguration;
+
+   @GetMapping("/hello")
+    public String HelloWorld(){
+       return String.format("applicationNameProperties: %s\napplicationNameConfiguration: %s",
+               applicationNameProperties,
+               applicationNameConfiguration);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
